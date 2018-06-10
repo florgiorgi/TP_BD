9ec37508f7c838b4c389e10c7104030996a19a04
Rocio <rdella@itba.edu.ar>
2018-06-09 22:26:05 -0300

    guia para importar

---
+++ importacion.sql
@@ -0,0 +1,30 @@
+-- Importación --
+
+-- Guia para importar a una tabla auxiliar los datos que estan en los csv --
+
+-- 1) Abrir DBVisualizer
+-- 2) Descargar los archivos csv de Campus
+-- 3) Mandar los archivos csv descargados a pampero. En Windows usar WinSCP. En Linux ejecutar el comando: 
+-- scp 'path-archivo-localhost' usuario@pampero.itba.edu.ar:path-archivo-remoto 
+-- Ejemplo: scp '/home/rocio/Descargas/test2.csv' rdella@pampero.itba.edu.ar:/home/rdella
+-- 4) Conectarse a la base de datos de pampero: ssh usuario@pampero.itba.edu.ar -L 5432:bd1.it.itba.edu.ar:5432
+-- 5) Una vez en la terminal de pampero, ejecutar psql -h bd1.it.itba.edu.ar -U usuario PROOF
+-- 6) En SQL Commander (DBVisualizer) y correrlo:
+
+SET datestyle = "ISO, DMY";
+
+CREATE TABLE datos_recorrido
+(periodo                TEXT,
+id_usuario              INTEGER NOT NULL,
+fecha_hora_retiro       TEXT NOT NULL,
+origen_estacion         INTEGER NOT NULL,
+nombre_origen           TEXT,
+destino_estacion        INTEGER NOT NULL,
+nombre_destino          TEXT,
+tiempo_uso              TEXT NOT NULL,
+fecha_creacion          DATE,
+PRIMARY KEY(id_usuario,fecha_hora_retiro, tiempo_uso));
+
+-- 7) En la terminal que tenemos abierta: PROOF => \copy datos_recorrido FROM '/home/rdella/test1.csv' csv header delimiter ';' NULL 'NULL';
+
+\copy datos_recorrido FROM 'path-archivo-remoto' csv header delimiter ';' NULL 'NULL';
