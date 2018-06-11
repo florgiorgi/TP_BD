557e6f1b3e8442fad7a45b59e146798f29c9a2dd
Alejandro Santoflaminio <asantoflaminio@itba.edu.ar>
2018-06-10 23:46:51 -0300

    hello

--- problem2.sql
+++ problem2.sql
@@ -2,7 +2,7 @@ CREATE OR REPLACE FUNCTION LIMPIA_REPETIDOS() RETURNS VOID
 AS $$
 
 DECLARE 
-    cursor1 CURSOR FOR SELECT DISTINCT id, fecha_hora FROM auxi ORDER BY tiempo_uso DESC;
+    cursor1 CURSOR FOR SELECT DISTINCT id_usuario, fecha_hora_retiro FROM datos_recorrido ORDER BY tiempo_uso DESC;
     
 
 REP RECORD;
@@ -11,7 +11,7 @@ begin
     LOOP
         FETCH cursor1 INTO REP;
         EXIT WHEN NOT FOUND;
-        PERFORM GUARDA(REP.id, REP.fecha_hora);
+        PERFORM GUARDA(REP.id_usuario, REP.fecha_hora_retiro);
     END LOOP;
 end;
 
@@ -20,11 +20,11 @@ $$LANGUAGE PLPGSQL;
 
 
 CREATE OR REPLACE FUNCTION GUARDA
-(myid auxi.id%TYPE, my_time auxi.fecha_hora%type) RETURNS VOID AS $$
+(myid datos_recorrido.id_usuario%TYPE, my_time datos_recorrido.fecha_hora_retiro%type) RETURNS VOID AS $$
 DECLARE
     mycursor CURSOR FOR
-    SELECT * FROM auxi
-    WHERE myid = id AND my_time = fecha_hora
+    SELECT * FROM datos_recorrido
+    WHERE myid = id_usuario AND my_time = fecha_hora_retiro
     ORDER BY tiempo_uso;
     
 REP RECORD;
@@ -33,19 +33,19 @@ CANT INT;
 BEGIN
  OPEN mycursor;
  FETCH mycursor INTO REP;
- CANT = 0;
+ CANT = 1;
 
 LOOP
  FETCH mycursor INTO REP;
  EXIT WHEN NOT FOUND;
- IF CANT == 2 THEN
- INSERT INTO RECORRIDO_FINAL VALUES(/*todas las columnas ?? */);
+ IF CANT == 2 THEN 
+ INSERT INTO RECORRIDO_FINAL VALUES(periodo , usuario , fecha_hora_ret , est_origen, est_destino, fecha_hora_dev, fecha_hora_ret);
  ELSE
  CANT = CANT + 1;
  END IF;
  END LOOP;
  IF CANT == 1 THEN
- INSERT INTO RECORRIDO_FINAL VALUES(/*todas las columnas ?? */);
+ INSERT INTO RECORRIDO_FINAL VALUES(periodo , usuario , fecha_hora_ret , est_origen, est_destino, fecha_hora_dev, fecha_hora_ret);
  CLOSE mycursor;
 END;
 $$ LANGUAGE PLPGSQL;
