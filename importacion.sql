90d949d938af58d273526c531494cf12407089ca
florgiorgi <mgiorgi@itba.edu.ar>
2018-06-16 01:58:06 -0300

    auxi2

--- importacion.sql
+++ importacion.sql
@@ -42,7 +42,7 @@ est_origen INTEGER NOT NULL,
 est_destino INTEGER NOT NULL,
 fecha_hora_dev TIMESTAMP NOT NULL CHECK(fecha_hora_dev >=
 fecha_hora_ret),
-PRIMARY KEY(usuario,fecha_hora_ret));
+PRIMARY KEY(periodo, usuario,fecha_hora_ret, est_origen, est_destino, fecha_hora_dev));
 
 CREATE TABLE recorrido_final
 (periodo TEXT,
